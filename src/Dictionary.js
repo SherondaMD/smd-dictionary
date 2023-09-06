2b4d9bbe2f24f3f2e15f06035df94ff8d77f9f49
SherondaMD <sheronda.mckee@gmail.com>
2023-09-05 19:47:38 -0700

    added axios and dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -8,9 +9,15 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for the definition of ${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   return (
